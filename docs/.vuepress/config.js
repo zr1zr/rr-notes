91e6018f497f486bf2edf6cd3d3ae60444548583
zr1zr <139022173+zr1zr@users.noreply.github.com>
2023-07-09 10:07:24 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -10,8 +10,25 @@ module.exports = {
   markdown: {
     lineNumbers: true, // 代码行号
   },
+  // vssue 评论插件
+  plugins: [
+    [
+      "vuepress-plugin-vssue-global",
+      {
+        platform: "github",
+        title: "[Comment]<%- frontmatter.title %>",
+        needComments: true,
+        // 其他的 Vssue 配置
+        autoCreateIssue: true,
+        clientId: "d3ec4ca6363950ca41a2",
+        clientSecret: "897465b6393f1d663e6128d2fab6959a0c0333cc",
+        owner: "lql95",
+        repo: "lql-notes",
+      },
+    ],
+  ],
+
 
   head,
-  plugins,
   themeConfig,
 }
